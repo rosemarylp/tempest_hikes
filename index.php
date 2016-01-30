58b314ee537b0c5078cee52f1d00b33219981e83
Rosemary <perkinsrosemaryl@gmail.com>
2016-01-29 17:10:20 -0800

    Add placehold comments to index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tempest Hikes</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<h1>Tempest Hikes</h1>
+		<!-- Search Bar -->
+	</header>
+	<main>
+		<h2>Hikes</h2>
+		<!-- Google map showing hikes as markers -->
+
+		<section class="recent-hikes">
+			<h2>Recent Hikes</h2>
+			<!-- This will be a feed of recent entries in the hiking guide, newest first -->
+			<!-- With a link to view the entire entry -->
+		</section>
+	</main>
+</body>
+</html>
