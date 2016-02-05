8e7ead84f66ff98739c6e27ce60e031e1dfeb6e6
Rosemary <perkinsrosemaryl@gmail.com>
2016-02-05 13:17:41 -0800

    Add infowindows to display hike_summary content

--- js/get_hikes.js
+++ js/get_hikes.js
@@ -10,6 +10,18 @@ $(document).ready(function() {
 			//var output = "<ul>";
 			for (var i = 0; i < data.rows.length; i++) {
 
+				var content_string = "<div>";
+				content_string += "<h3>" + data.rows[i].value.name + "</h3>";
+				content_string += "<p>";
+				content_string += "<strong>Distance:</strong>" + data.rows[i].value.distance + "</p>";
+				content_string += "<img src=\"http://127.0.0.1:5984/tempest_hikes/" + data.rows[i].id + "/" + data.rows[i].value.attachments[0] + "\" height=200>";
+				content_string += "</div>";
+
+				var infowindow = new google.maps.InfoWindow({
+					content:content_string
+				});
+
+
 				var this_hike_location = new google.maps.LatLng(data.rows[i].value.lat, data.rows[i].value.long);
 
 				var marker = new google.maps.Marker({
@@ -17,6 +29,10 @@ $(document).ready(function() {
 				});
 
 				marker.setMap(map);
+
+				marker.addListener('click', function() {
+					infowindow.open(map, marker);
+				})
 
 				// output += "<li>" + data.rows[i].value.name + "<ul>";
 				// output += "<li>" + data.rows[i].value.location + "</li>";
