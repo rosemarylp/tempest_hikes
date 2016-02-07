c9f57f36fad757be75b35e016b207238a3d1edee
Rosemary <perkinsrosemaryl@gmail.com>
2016-02-06 18:29:16 -0800

    On click, markers will show hike information in external section in place of infowindow. No styling.

--- js/get_hikes.js
+++ js/get_hikes.js
@@ -9,52 +9,37 @@ $(document).ready(function() {
 			console.log(typeof data);
 			//var output = "<ul>";
 			for (var i = 0; i < data.rows.length; i++) {
-
-				var content_string = "<div>";
-				content_string += "<h3>" + data.rows[i].value.name + "</h3>";
-				content_string += "<p>";
-				content_string += "<strong>Distance:</strong>" + data.rows[i].value.distance + "</p>";
-				content_string += "<img src=\"http://127.0.0.1:5984/tempest_hikes/" + data.rows[i].id + "/" + data.rows[i].value.attachments[0] + "\" height=200>";
-				content_string += "</div>";
-
-				var infowindow = new google.maps.InfoWindow({
-					content:content_string
-				});
-
 				var this_lat = data.rows[i].key.lat;
-				var this_lng = data.rows[i].key.long;
+				var this_lng = data.rows[i].key.lng;
 
 				var this_hike_location = new google.maps.LatLng(this_lat, this_lng);
-
 
 				var marker = new google.maps.Marker({
 					position:this_hike_location,
 				});
-				show_info(marker,this_lat,this_lng);
-				marker.setMap(map);
+				
+				//Adds click event listener to each marker
+				add_hike_info(marker,this_lat,this_lng);
 
-				// output += "<li>" + data.rows[i].value.name + "<ul>";
-				// output += "<li>" + data.rows[i].value.location + "</li>";
-				// output += "<li>" + data.rows[i].value.distance + "</li>";
-				// for (var j=0; j < data.rows[i].value.attachments.length; j++) {
-				// 	output += "<img src=\"http://127.0.0.1:5984/tempest_hikes/" + data.rows[i].id + "/" + data.rows[i].value.attachments[j] + "\" height=200>";
-				// }
-				// output += "</ul>";
-				// output += "</li>";
+				marker.setMap(map);
 			}
-			function show_info(marker,this_lat,this_lng) {
+
+			function add_hike_info(marker,this_lat,this_lng) {
+				//Adds click event listener to each marker
 				marker.addListener('click', function() {
 					for (var j=0; j < data.rows.length; j++) {
 
-						if (data.rows[j].key.lat == this_lat && data.rows[j].key.long == this_lng) {
-							alert(data.rows[j].value.name);
-						}
-					}
-
+						if (data.rows[j].key.lat == this_lat && data.rows[j].key.lng == this_lng) {
+							var output = "<h3>" + data.rows[j].value.name + "</h3>";
+							output += "<h4>Distance: " + data.rows[j].value.distance + "</h4>";
+							for (var k=0; k < data.rows[j].value.attachments.length; k++) {
+								output += "<img src=\"http://127.0.0.1:5984/tempest_hikes/" + data.rows[j].id + "/" + data.rows[j].value.attachments[k] + "\" height=200>";
+							} //end for
+							$('#hike_info').html(output);
+						} //endif
+					} //end for
 				});
-			}
-			//output += "</ul>";
-			//$('#hike_summaries').append(output);
+			} //end add_hike_info
 		});
 	}
 
