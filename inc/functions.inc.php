74f9883455ea3fef46c9d6e7536f4655c5a911cf
Rosemary <perkinsrosemaryl@gmail.com>
2016-02-18 17:42:40 -0800

    submit_form puts hike information to db

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -1,5 +1,9 @@
 <?php 
 
+// require_once 'connect.inc.php';
+// echo $db_user;
+// echo $db_pass;
+
 function call_db($url) {
 	$ch = curl_init();
 
@@ -11,6 +15,51 @@ function call_db($url) {
 	curl_close($ch);
 
 	return $result;
+}
+
+function put_db($fields) {
+	require_once 'connect.inc.php';
+
+
+	$id = get_id();
+
+	$url = "http://127.0.0.1:5984/tempest_hikes/" . $id;
+
+	$data = json_encode($fields);
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data)));
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	echo "Status: " . $status_code;
+	$response = curl_exec($ch);
+
+	return $response;
+
+	curl_close($ch);
+
+}
+
+function get_id() {
+	$url = "http://127.0.0.1:5984/_uuids";
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$response = curl_exec($ch);
+
+	$response = json_decode($response);
+
+	$id = $response->uuids[0];
+
+	return $id;
 }
 
 
--- inc/submit_form.inc.php
+++ inc/submit_form.inc.php
@@ -8,45 +8,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 	if (isset($_POST['hike_name'])) {
 		$hike_name = $_POST['hike_name'];
-		array_push($fields,"name"=>$hike_name);
+		$fields["name"] = $hike_name;
 	}
 
 	if (isset($_POST['area'])) {
 		$area = $_POST['area'];
-		array_push($fields,"area"=>$area);
+		$fields["area"] = $area;
 	}
 
 	if (isset($_POST['date'])) {
 		$date = $_POST['date'];
-		array_push($fields, "date"=>$date);
+		$fields["date"] = $date;
 	}
 
 	if (isset($_POST['description'])) {
 		$description = $_POST['description'];
-		array_push($fields, "description"=>$description);
+		$fields["description"] = $description;
 	}
 
 	if (isset($_POST['distance'])) {
 		$distance = $_POST['distance'];
-		array_push($fields, "distance"=>$distance);
+		$fields["distance"] = $distance;
 	}
 
 	if (isset($_POST['elevation_gain'])) {
 		$elevation_gain = $_POST['elevation_gain'];
-		array_push($fields, "elevation_gain"=>$elevation_gain);
+		$fields["elevation_gain"] = $elevation_gain;
 	}
 
 	if (isset($_POST['lat'])) {
 		$lat = $_POST['lat'];
-		array_push($fields, "lat"=>$lat);
+		$fields["lat"] = $lat;
 	}
 
 	if (isset($_POST['lng'])) {
 		$lng = $_POST['lng'];
-		array_push($fields, "lng"=>$lng);
+		$fields["lng"] = $lng;
 	}
 
 	$result = put_db($fields);
+	echo $result;
 }
 
 
