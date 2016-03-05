e02d921a97657b052753571441199043c3672956
Rosemary <perkinsrosemaryl@gmail.com>
2016-03-04 21:16:10 -0800

    add delete function to functions.inc.php

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -37,6 +37,24 @@ function put_db($fields, $url) {
 	curl_close($ch);
 }
 
+function delete_db($url) {
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Content-type: application/json',
+		'Accept: */*')
+	);
+
+	$response = curl_exec($ch);
+
+	echo $response;
+
+	curl_close($ch);
+}
+
 function put_attachment($result, $attachment) {
 	$doc_info = get_doc_info($result);
 	$doc_id = $doc_info["id"];
